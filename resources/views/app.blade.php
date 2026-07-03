ab195b4ea56386f651ec60ea11cd59b5f19243b9
Bipin Sahani <107175682+bipinsahani786@users.noreply.github.com>
2026-07-03 11:48:04 +0530

    fix: remove favicon.ico link to force google to use svg

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -46,7 +46,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-        <link rel="icon" href="/favicon.ico?v=2" sizes="any">
+        {{-- <link rel="icon" href="/favicon.ico?v=2" sizes="any"> --}}
         <link rel="icon" href="data:image/svg+xml;base64,{{ base64_encode(file_get_contents(public_path('favicon.svg'))) }}" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=2">
 
