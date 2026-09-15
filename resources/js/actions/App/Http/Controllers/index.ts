4a368ebd68065ebb8963fb7a01e3d799785f6b72
SocialAcceptare <sahilkumar242004@gmail.com>
2026-09-15 16:27:56 +0530

    fix(ci): pin PHP 8.4 in tests workflow and add safe optional chaining for auth user in SSR

--- resources/js/actions/App/Http/Controllers/index.ts
+++ resources/js/actions/App/Http/Controllers/index.ts
@@ -1,10 +1,13 @@
-import SeoController from './SeoController'
-import CaseStudyController from './CaseStudyController'
-import Settings from './Settings'
+import SeoController from './SeoController';
+import CaseStudyController from './CaseStudyController';
+import Settings from './Settings';
 const Controllers = {
     SeoController: Object.assign(SeoController, SeoController),
-CaseStudyController: Object.assign(CaseStudyController, CaseStudyController),
-Settings: Object.assign(Settings, Settings),
-}
+    CaseStudyController: Object.assign(
+        CaseStudyController,
+        CaseStudyController,
+    ),
+    Settings: Object.assign(Settings, Settings),
+};
 
-export default Controllers
+export default Controllers;
--- resources/js/routes/blog/index.ts
+++ resources/js/routes/blog/index.ts
@@ -1,96 +1,124 @@
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../wayfinder'
+import {
+    queryParams,
+    type RouteQueryOptions,
+    type RouteDefinition,
+    type RouteFormDefinition,
+    applyUrlDefaults,
+} from './../../wayfinder';
 /**
  * @see routes/web.php:44
  * @route '/blog/{slug}'
  */
-export const details = (args: { slug: string | number } | [slug: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+export const details = (
+    args: { slug: string | number } | [slug: string | number] | string | number,
+    options?: RouteQueryOptions,
+): RouteDefinition<'get'> => ({
     url: details.url(args, options),
     method: 'get',
-})
+});
 
 details.definition = {
-    methods: ["get","head"],
+    methods: ['get', 'head'],
     url: '/blog/{slug}',
-} satisfies RouteDefinition<["get","head"]>
+} satisfies RouteDefinition<['get', 'head']>;
 
 /**
  * @see routes/web.php:44
  * @route '/blog/{slug}'
  */
-details.url = (args: { slug: string | number } | [slug: string | number ] | string | number, options?: RouteQueryOptions) => {
+details.url = (
+    args: { slug: string | number } | [slug: string | number] | string | number,
+    options?: RouteQueryOptions,
+) => {
     if (typeof args === 'string' || typeof args === 'number') {
-        args = { slug: args }
+        args = { slug: args };
     }
 
-    
     if (Array.isArray(args)) {
         args = {
-                    slug: args[0],
-                }
+            slug: args[0],
+        };
     }
 
-    args = applyUrlDefaults(args)
+    args = applyUrlDefaults(args);
 
     const parsedArgs = {
-                        slug: args.slug,
-                }
+        slug: args.slug,
+    };
 
-    return details.definition.url
+    return (
+        details.definition.url
             .replace('{slug}', parsedArgs.slug.toString())
             .replace(/\/+$/, '') + queryParams(options)
-}
+    );
+};
 
 /**
  * @see routes/web.php:44
  * @route '/blog/{slug}'
  */
-details.get = (args: { slug: string | number } | [slug: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+details.get = (
+    args: { slug: string | number } | [slug: string | number] | string | number,
+    options?: RouteQueryOptions,
+): RouteDefinition<'get'> => ({
     url: details.url(args, options),
     method: 'get',
-})
+});
 /**
  * @see routes/web.php:44
  * @route '/blog/{slug}'
  */
-details.head = (args: { slug: string | number } | [slug: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'head'> => ({
+details.head = (
+    args: { slug: string | number } | [slug: string | number] | string | number,
+    options?: RouteQueryOptions,
+): RouteDefinition<'head'> => ({
     url: details.url(args, options),
     method: 'head',
-})
+});
 
-    /**
+/**
  * @see routes/web.php:44
  * @route '/blog/{slug}'
  */
-    const detailsForm = (args: { slug: string | number } | [slug: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-        action: details.url(args, options),
-        method: 'get',
-    })
+const detailsForm = (
+    args: { slug: string | number } | [slug: string | number] | string | number,
+    options?: RouteQueryOptions,
+): RouteFormDefinition<'get'> => ({
+    action: details.url(args, options),
+    method: 'get',
+});
 
-            /**
+/**
  * @see routes/web.php:44
  * @route '/blog/{slug}'
  */
-        detailsForm.get = (args: { slug: string | number } | [slug: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-            action: details.url(args, options),
-            method: 'get',
-        })
-            /**
+detailsForm.get = (
+    args: { slug: string | number } | [slug: string | number] | string | number,
+    options?: RouteQueryOptions,
+): RouteFormDefinition<'get'> => ({
+    action: details.url(args, options),
+    method: 'get',
+});
+/**
  * @see routes/web.php:44
  * @route '/blog/{slug}'
  */
-        detailsForm.head = (args: { slug: string | number } | [slug: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-            action: details.url(args, {
-                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
-                            _method: 'HEAD',
-                            ...(options?.query ?? options?.mergeQuery ?? {}),
-                        }
-                    }),
-            method: 'get',
-        })
-    
-    details.form = detailsForm
+detailsForm.head = (
+    args: { slug: string | number } | [slug: string | number] | string | number,
+    options?: RouteQueryOptions,
+): RouteFormDefinition<'get'> => ({
+    action: details.url(args, {
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'HEAD',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        },
+    }),
+    method: 'get',
+});
+
+details.form = detailsForm;
 const blog = {
     details: Object.assign(details, details),
-}
+};
 
-export default blog
+export default blog;
